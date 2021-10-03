34f5a3705dbfff02a52787d7a79015ca46b1d32f
Daniel Tremura <tremuradaniel@gmail.com>
2021-10-03 15:41:31 +0300

    keep user logged in even after page refresh vuex-persistedstate

--- src/store/main.js
+++ src/store/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
 
 Vue.use(Vuex)
 
@@ -22,5 +24,12 @@ export default new Vuex.Store({
   },
   modules:{
     auth
-  }
+  },
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })],
 })
